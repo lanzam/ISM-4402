a1c7a2f04225b4b335e0adf8b4c6e45c0dc8672d
lanzam <46579795+lanzam@users.noreply.github.com>
2019-09-09 22:33:34 -0400

    Add files via upload

---
+++ USCensusPoverty.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd 
+
+Location = "PVY02.xls"
+df = pd.read_excel(Location)
+
+
+# In[5]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
