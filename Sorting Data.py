b1e4764dcada0c6f3b9150b72c41c693c2d40226
lanzam <46579795+lanzam@users.noreply.github.com>
2019-10-14 22:04:18 -0400

    Add files via upload

---
+++ Sorting Data.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd
+Location = "Datasets/gradedata.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[6]:
+
+
+df = df.sort_values(by=['lname','age','grade'], ascending=[True,True,False])
+df.head()
+
+
+# In[ ]:
+
+
+
+
